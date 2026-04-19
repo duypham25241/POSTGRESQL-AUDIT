bb536d3c0a1afd02d42c9ba7de1b4477d785cfe1
duypham25241 <140091136+duypham25241@users.noreply.github.com>
2026-04-19 09:12:19 +0700

    Boc tach chi phi

--- tach chiphi.sql
+++ tach chiphi.sql
@@ -1,4 +1,4 @@
--- Tao bang tam chi phi
+-- Tao bang tam Cost_table 
 create table Cost_table(
 	don_vi varchar(1024),
 	nhom_doi_tuong varchar(1024),
@@ -35,6 +35,26 @@ insert into Cost_table (
 	phat_sinh_co_nguyen_te,
 	phat_sinh_no_vnd,
 	phat_sinh_co_vnd)
+
+select 
+	don_vi,
+	nhom_doi_tuong,
+	ma_ncc,
+	ten_ncc,
+	quoc_gia,
+	so_chung_tu,
+	so_hoa_don,
+	ngay_chung_tu,
+	loai_ct,
+	dien_giai,
+	ty_gia,
+	don_vi_tien,
+	phat_sinh_no_nguyen_te,
+	phat_sinh_co_nguyen_te,
+	phat_sinh_no_vnd,
+	phat_sinh_co_vnd
+from AP004 
+	
 	
 -- Data Cost_table 
 select 
@@ -58,3 +78,62 @@ from
 	from cost_table ct )
 where Loaigd is null
 group by ten_ncc 
+
+--------------------------------------------------------------------------------------------------
+-- Check do dai ky tu trong ID NCC (7 so)
+select 
+	ma_ncc 
+from ncc
+where length(ma_ncc) <> 7 and ma_ncc <> 'Chưa tìm ra'
+
+-- Check data loi co ky tu trong ID NCC	
+select 
+	ma_ncc::int
+from 
+	ncc
+where ten_ncc not ilike 'Chưa tìm ra'
+
+-- Check duplicate trong bang 
+select 
+	mat_hang,
+	ma_ncc,
+	ten_ncc,
+	ten_viet_tat,
+	htht,
+	thuong_hieu,
+	count(*)
+from ncc
+group by mat_hang, ma_ncc, ten_ncc, ten_viet_tat, htht, thuong_hieu
+having count(*) > 1
+
+-- Doi chieu giua bang data moi va cu
+select 
+	*
+from ncc_xlsx a
+where not exists
+	(select 
+		1 
+	from ncc b
+	where a.ma_ncc = b.ma_ncc 
+	)
+
+-- Them bang data NCC moi tam vao truoc, update lai bang goc
+-- Cap nhat data data moi
+-- Update ma_NCC moi bang data moi 
+update ncc a
+	set ma_ncc = b.ma_ncc 
+from ncc_xlsx b
+where a.ten_viet_tat = b.ten_viet_tat  
+	and a.ma_ncc like '%0.0%'
+
+-- Update data loi ky tu
+update ncc a
+	set ma_ncc = replace(trim(b.ma_ncc),'.0','')
+from ncc_xlsx b
+where a.ten_viet_tat = b.ten_viet_tat  
+
+-- Check lai sau update
+select 
+	ma_ncc::int
+from ncc
+where ten_ncc not ilike 'Chưa tìm ra'
